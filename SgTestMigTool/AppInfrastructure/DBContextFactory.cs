78b51f0d85c84cf9ab320baf2fcfd8bec2e110a4
Aleksey . <exaybo@hotmail.com>
2025-04-15 14:28:49 +0500

    docker

--- SgTestMigTool/AppInfrastructure/DBContextFactory.cs
+++ SgTestMigTool/AppInfrastructure/DBContextFactory.cs
@@ -6,16 +6,16 @@ namespace AppInfrastructure
     public class DBContextFactory : IDesignTimeDbContextFactory<AppDbContext>
     {
         //useful for migration
-        //  Add-Migration <name> -project appdbstuff -startup appdbstuff
-        //  Update-Database -project appdbstuff -startup appdbstuff
+        //  Add-Migration <name> -project AppInfrastructure -startup AppInfrastructure
+        //  Update-Database -project AppInfrastructure -startup AppInfrastructure
 
         //clear Migrations dir
-        //Drop-Database -project appdbstuff -startup appdbstuff
+        //  Drop-Database -project AppInfrastructure -startup AppInfrastructure
 
         public AppDbContext CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=sgTestStruct;User Id=admin;Password=Wr()0m-wRo0M;Include Error Detail=true;")
+            optionsBuilder.UseNpgsql("Server=migdb;Port=5432;Database=migtest;User Id=migrator;Password=Wr()0m-wRo0M;Include Error Detail=true;")
                 .EnableSensitiveDataLogging();
 
             return new AppDbContext(optionsBuilder.Options);
